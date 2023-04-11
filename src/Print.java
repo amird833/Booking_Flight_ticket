642503e3b88c9a6c5476c08edcdf527b0bfa8d9c
Amir <adaghaghpoor@gmail.com>
2023-04-11 22:18:54 +0430

    fix printFlight bug

--- src/Print.java
+++ src/Print.java
@@ -113,7 +113,7 @@ public class Print {
         int index =0;
         int flag = 0;
         for (int i = 0; i < flights.length; i++) {
-            if(flights[i].getFlightId().equals(flightId))
+            if(flights[i].getFlightId()!=null && flights[i].getFlightId().equals(flightId))
             {
                 flag = 1;
                 index = i;
