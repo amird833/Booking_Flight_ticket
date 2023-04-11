e0be8b275b4a84d2cedcc324ee1b1047e5a9f792
Amir <adaghaghpoor@gmail.com>
2023-04-11 10:21:40 +0430

    add main methods and finish first switch case

--- src/Methods.java
+++ src/Methods.java
@@ -1,2 +1,16 @@
 public class Methods {
+    Print print = new Print();
+    public void main()
+    {
+        while (true)
+        {
+            switch (print.printSignMenu())
+            {
+                case 1 :
+                    break;
+                case 2 :
+                    break;
+            }
+        }
+    }
 }
