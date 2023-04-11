266b38bc606837a6099551facdc6ae15d2b66c34
Amir <adaghaghpoor@gmail.com>
2023-04-11 10:26:12 +0430

    update printSignMenu methods for dosent access incorrect choice

--- src/Print.java
+++ src/Print.java
@@ -4,9 +4,17 @@ public class Print {
     Scanner input = new Scanner(System.in);
     public int printSignMenu()
     {
+        int chose;
         System.out.println("1-Sign in");
         System.out.println("2- Sign up");
         System.out.printf(">>");
-        return input.nextInt();
+        chose = input.nextInt();
+        while (chose > 2 || chose < 1)
+        {
+            System.out.println("Try again!");
+            System.out.printf(">>");
+            chose = input.nextInt();
+        }
+        return chose;
     }
 }
