b8dda6b3b6e29faa98831a1338292caf5cfca69d
Amir <adaghaghpoor@gmail.com>
2023-04-11 11:25:42 +0430

    add printAdminOption methode to Print class

--- src/Print.java
+++ src/Print.java
@@ -12,7 +12,25 @@ public class Print {
         while (chose > 2 || chose < 1)
         {
             System.out.println("Try again!");
-            System.out.printf(">>");
+            System.out.print(">>");
+            chose = input.nextInt();
+        }
+        return chose;
+    }
+    public int printAdminOption()
+    {
+        int chose;
+        System.out.println("1- Add");
+        System.out.println("2- Update");
+        System.out.println("3- Remove");
+        System.out.println("4 - Flight schedules");
+        System.out.println("0 - Sign out");
+        System.out.print(">>");
+        chose = input.nextInt();
+        while (chose > 4 || chose<0)
+        {
+            System.out.println("Try again!");
+            System.out.print(">>");
             chose = input.nextInt();
         }
         return chose;
