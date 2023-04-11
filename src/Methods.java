58253cb41061ce1b5c1058ea461ce0e0c30539b2
Amir <adaghaghpoor@gmail.com>
2023-04-11 10:58:52 +0430

    add addUser methods for give username and pass ,but get a null exception :(

--- src/Methods.java
+++ src/Methods.java
@@ -1,7 +1,43 @@
+import java.util.Scanner;
+
 public class Methods {
+    Scanner input = new Scanner(System.in);
     Print print = new Print();
-    public void main()
+    User[] users = new User[20];
+    public void addUser(int count)
     {
+        String userStr="";
+        int flag = 1;
+        int flagUser ;
+        System.out.print("Username : ");
+        while (flag == 1)
+        {
+            flagUser = 0;
+            userStr = input.next();
+
+            for (User user : users) {
+                if (user.getUsername() != null && user.getUsername().equals(userStr)) {
+                    flagUser = 1;
+                    break;
+                }
+
+            }
+            if (flagUser != 1 )
+            {
+                flag = 0;
+            }
+            else
+            {
+                System.out.println("Incorrect!");
+                System.out.println("Try again ");
+                System.out.print("Username : ");
+            }
+        }
+        users[count].setUsername(userStr);
+    }
+    public void start()
+    {
+        int userCount = 0;
         while (true)
         {
             switch (print.printSignMenu())
@@ -9,6 +45,10 @@ public class Methods {
                 case 1 :
                     break;
                 case 2 ://sabte num
+                    addUser(userCount);
+                    System.out.print("Password : ");
+                    users[userCount].setPassword(input.next());
+                    userCount++;
                     break;
             }
         }
