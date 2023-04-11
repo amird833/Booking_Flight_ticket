dad9d566f3be680a74065aeebd1a1627aaf19367
Amir <adaghaghpoor@gmail.com>
2023-04-11 10:56:57 +0430

    add getter and setter to User class

--- src/User.java
+++ src/User.java
@@ -2,19 +2,19 @@ public class User {
     private String username;
     private String password;
 
-    public String getUserName() {
+    public String getUsername() {
         return username;
     }
 
-    public void setUserName(String userName) {
-        this.username = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getPassWord() {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassWord(String passWord) {
-        this.password = passWord;
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
